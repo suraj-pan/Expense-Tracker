cfb3ad3c2a6ab72487475c48e3ce1d42498facab
suraj-pan <surajpandey82212@gmail.com>
2024-02-12 19:34:01 +0530

    sign-up page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SignUp from "./components/SignUp";
+
+function App() {
+  return (
+    <div className="w-full h-full">
+    <SignUp/>
+
+    </div>   
+  );
+}
+
+export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,64 @@
+import React, { useRef } from 'react'
+
+const SignUp = () => {
+
+    const email = useRef();
+    const password = useRef();
+    const confirmPassword = useRef();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        if (password.current.value !== confirmPassword.current.value) {
+            alert("password does not match")
+        }
+
+        try {
+            const sendData = async (email,password) => {
+                const response = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDqssr-XYkQE5aR15B7bVhhygLUnqGrHbc", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                        returnSecureToken: true
+                    }),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+
+                const data = await response.json();
+
+                console.log(data);
+
+
+            }
+
+            sendData(email.current.value, password.current.value)
+        } catch (error) {
+            console.error("ERROR", error)
+        }
+
+
+    }
+
+    return (
+        <div className='flex flex-col justify-center items-center '>
+            <h2 className='text-2xl text-center font-bold' >Sign-Up</h2>
+            <form className='flex flex-col ' onSubmit={submitHandler}>
+                <label>Name:
+                    <input type='email' ref={email} placeholder='enter your email' required />
+                </label>
+                <label>Password:
+                    <input type='password' ref={password} placeholder='enter your password' required />
+                </label>
+                <label>Confirm Password:
+                    <input type='password' ref={confirmPassword} placeholder='enter your confirm password' required />
+                </label>
+                <button type='submit' >Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp
